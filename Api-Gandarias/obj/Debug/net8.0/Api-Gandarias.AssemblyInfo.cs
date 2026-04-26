3419af1bfd06e779b59a67e792ae541fc72eca79
Alex Diaz <diazbetancur@gmail.com>
2026-04-26 08:53:31 -0500

    update

--- Api-Gandarias/obj/Debug/net8.0/Api-Gandarias.AssemblyInfo.cs
+++ Api-Gandarias/obj/Debug/net8.0/Api-Gandarias.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Api-Gandarias")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0dc03747c0420dae857c99df3261ddb33ce6784c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Api-Gandarias")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Api-Gandarias")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
